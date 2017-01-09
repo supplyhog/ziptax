81eb39d556fffac385f2a96eca957388a80ee671
Philip Brown <philip@supplyhog.com>
2017-01-09 10:33:44 -0500

    Updated README and example for v2.

--- README.md
+++ README.md
@@ -5,17 +5,24 @@
 ## Requirements
 
 1. An account on zip-tax.com
-2. PHP 5.3.0+
+2. PHP 5.6.0+
 3. libcurl
 
 ## Usage
 
-```php
-$zipTax = new vutran\ZipTax('YOUR_API_KEY');
+```
+<?php
+$zipTax = new supplyhog\ZipTax('YOUR_API_KEY');
 
-$rate = $zipTax->request('90210');
-
-echo $rate->results[0]->taxSales;
+try{
+	$response = $zipTax->request('37402', 'Chattanooga', 'TN');
+	if($response) {
+		echo $response['results'][0]['taxSales'];
+	}
+}
+catch(\Exception $e) {
+	echo $e->getMessage();
+}
 ```
 
 ## License
--- examples/index.php
+++ examples/index.php
@@ -3,13 +3,43 @@
 require_once('../vendor/autoload.php');
 
 // Set the API key
-$apiKey = 'YOUR_API_KEY';
+$apiKey = 'YOUR-API-KEY';
 
 // Instantiate the client
-$zipTax = new vutran\ZipTax($apiKey);
+$zipTax = new supplyhog\ZipTax($apiKey);
 
-$rate = $zipTax->request('90210');
+//  Valid Call
+try{
+	$response = $zipTax->request('37402', 'Chattanooga', 'TN');
+	var_dump($response);
+}
+catch(\Exception $e) {
+	var_dump($e->getMessage());
+}
 
-print_r($rate);
+//  Throws an Error: Invalid State
+try{
+	$response = $zipTax->request('37402', 'Chattanooga', 'Tennessee');
+	var_dump($response);
+}
+catch(\Exception $e) {
+	var_dump($e->getMessage());
+}
 
-?>
+//  Returns false.  Invalid City.
+try{
+	$response = $zipTax->request('37402', 'Somewhere Outthere', 'TN');
+	var_dump($response);
+}
+catch(\Exception $e) {
+	var_dump($e->getMessage());
+}
+
+//  Postal Code only.  This may return more than one valid result.
+try{
+	$response = $zipTax->request('90210');
+	var_dump($response);
+}
+catch(\Exception $e) {
+	var_dump($e->getMessage());
+}
